e1319d0b66c8b30ad695377bd10d4f804be23fbe
Sahil Prasad <prasadsahil06@gmail.com>
2024-09-22 20:38:13 +0530

    Error classes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import "dotenv/config";
-
-import express from "express";
 import { dataBase } from "./db/database.js";
-
-dataBase();
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Hello Citrawarta");
-});
-app.listen(process.env.PORT, () => {
-  console.log(
-    `The app is litening on port ${process.env.PORT} and this is the link http://127.0.0.1:8000/`
-  );
-});
+import { app } from "./app.js";
+dataBase()
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(
+        `Citrawarta is listeinig on port ${process.env.PORT || 8000}`
+      );
+    });
+  })
+  .catch((error) => {
+    console.error(error);
+  });
