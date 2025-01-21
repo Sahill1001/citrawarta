644d02a8bb3439150e2b86fc5b8c4823aeeee32c
Sahil Prasad <sahilkumar.prasad@crescentengg.co.in>
2025-01-22 01:46:29 +0530

    Tweet controller

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -1,6 +1,5 @@
 import mongoose, { isValidObjectId } from "mongoose";
 import { Tweet } from "../models/tweet.model.js";
-import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
@@ -72,7 +71,7 @@ const deleteTweet = asyncHandler(async (req, res) => {
         throw new ApiError(404, "Tweet not found");
     }
 
-    retuen res.status(200).json(new ApiResponse(200, tweet, "Tweet deleted"));
+    return res.status(200).json(new ApiResponse(200, tweet, "Tweet deleted"));
 });
 
 export { createTweet, getUserTweets, updateTweet, deleteTweet };
